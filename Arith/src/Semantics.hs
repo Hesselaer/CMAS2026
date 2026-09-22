0204681fd9d293440bc633d3f6c742b2062882f6
Hessellaar <hessel.sieburgh@gmail.com>
2026-09-22 11:40:23 +0200

    smallstep semantics

--- Arith/src/Semantics.hs
+++ Arith/src/Semantics.hs
@@ -33,7 +33,7 @@ denotation env e = case e of
   ENum   k     -> Just k
   EVar   ident -> Just $ getVal env ident
   ETimes e1 e2 -> liftM2 (*) (denotation env e1) (denotation env e2)
-  EDiv   e1 e2 -> if (denotation env e2) == 0 then Nothing else liftM2 (/) (denotation env e1) (denotation env e2)
+  EDiv   e1 e2 -> if (denotation env e2) == Just 0 then Nothing else liftM2 (/) (denotation env e1) (denotation env e2)
   EPlus  e1 e2 -> liftM2 (+) (denotation env e1) (denotation env e2)
 
 -- | Implements the big-step operational semantics. Note that bigStep can be a function because
@@ -56,7 +56,7 @@ smallStep (env, e) = case e of
   ENum   _     -> empty
   EVar   ident -> singleton (env, ENum $ getVal env ident)
   ETimes e1 e2 -> binaryS e1 e2 (\x -> Just . (*) x) ETimes
-  EDiv   e1 e2 -> failure e --TODO
+  EDiv   e1 e2 -> binaryS e1 e2 (\x -> \y -> if y == 0 then Nothing else Just (x / y)) EDiv
   EPlus  e1 e2 -> binaryS e1 e2 (\x -> Just . (+) x) EPlus
 
   where
